b5a04c30b24b42499b64c805278a35dbc8461bd8
Tozo <viniciusdelimatozo@hotmail.com>
2018-11-11 16:52:10 -0200

    Mudança de nome e correção da navbar
    Mudei o nome da caixa de entrada pra inbox.html, mail.html agora é a visualização de um email específico

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@
 // o login e senha que o usuário digitar
 // e receber se o login foi efetuado com 
 // sucesso, e redirecionar o usuário pra 
-// caixa de entrada (paginas/mail.html)
+// caixa de entrada (paginas/inbox.html)
--- js/nav.js
+++ js/nav.js
@@ -15,5 +15,21 @@ $(document).ready(function(){
 		}
 		
 	});
+
+	$(window).resize(function(){
+		var viewportWidth = $(window).width();
+
+		if(viewportWidth>600){	
+	        $("nav").css({width: "260px"}, 500);
+			$(".nav_item").css({width: "100%"}, 500);
+			$(".ativo").css({width: "270px"}, 500);
+			nav_visivel = true;
+		}
+		else{
+			$("nav").css({width: "0"}, 500);
+			$(".nav_item").css({width: "0"}, 500);
+			nav_visivel = false;
+		}
+    });
 	
 });
