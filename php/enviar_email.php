416622ce82cf9d37a0c579386645592d5e62c431
Tozo <viniciusdelimatozo@hotmail.com>
2018-11-15 14:47:03 -0200

    Update php/enviar_email.php
    Adicionei remetente, tem que mudar depois com o email na sessao

--- php/enviar_email.php
+++ php/enviar_email.php
@@ -2,6 +2,7 @@
 	$id = uniqid();
 	// Gera um id único pro email, para dar pra excluir e para guardar em arquivos diferentes
 
+	$remetente    = "email@gmail.com"; // MUDAR QUANDO TIVER A SESSAO (LOGIN)
 	$destinatario = $_POST["destinatario"];
 	$assunto      = $_POST["assunto"];
 	$conteudo     = $_POST["conteudo"];
@@ -9,6 +10,7 @@
 	$xml = new DOMDocument("1.0");
 	$tag_emailEnviado = $xml->createElement("email");
 	$tag_destinatario = $xml->createElement("destinatario", $destinatario);
+	$tag_remetente= $xml->createElement("remetente", $remetente);
 	$tag_assunto = $xml->createElement("assunto", $assunto);
 	$tag_conteudo = $xml->createElement("conteudo", $conteudo);
 	$tag_id = $xml->createElement("id", $id);
@@ -17,6 +19,7 @@
 	$tag_emailEnviado->appendChild($tag_id);
 	$tag_emailEnviado->appendChild($tag_assunto);
 	$tag_emailEnviado->appendChild($tag_destinatario);
+	$tag_emailEnviado->appendChild($tag_remetente);
 	$xml->appendChild($tag_emailEnviado);
 
 	$xml->save("../arquivos/emails/email_".$id.".xml");
