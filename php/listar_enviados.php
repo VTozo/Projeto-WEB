942c11cf631852c4b1f9810d125b9267b6260fdb
DiogoFeld <43175839+DiogoFeld@users.noreply.github.com>
2018-11-17 14:37:27 -0200

    uptade
    estou tendo alguns problemas em Js e php, vou mandar umas mensagens

--- php/listar_enviados.php
+++ php/listar_enviados.php
@@ -4,24 +4,23 @@ $emails = file_get_contents("../arquivos/emails/");
 $diretorio = dir($emails);
 $array = array();
 
-//usuarios:
-//ver xml dos usuarios
+//
+
 
 
 	while($file=$diretorio ->read()){
-		if ($file !="." && $file!=".."){//add id de usuario via xml
-			$xml_string = file_get_contents($emails.$file);
-			$xml_object = simplexml_load_string($xml_string);
-
-
-			$array = array(
-				'id'        => (string)$xml_object->id,
-          		'conteudo'  => (string)$xml_object->conteudo,
-            	'assunto'   => (string)$xml_object->assunto,
-				'remetente' => (string)$xml_object->remetente
-        	);							
+	if ($file !="." && $file!=".."&& $_SESSION== {//remetente
+	$xml_string = file_get_contents($emails . $file);
+	$xml_object = simplexml_load_string($xml_string);
+	
+	$array = array(
+		'id'        => (string)$xml_object->id,
+  		'conteudo'  => (string)$xml_object->conteudo,
+      	'assunto'   => (string)$xml_object->assunto,
+		'remetente' => (string)$xml_object->remetente
+      	);							
 		}
-	}
+}
 	$diretorio->close();
 
 	$json = json_encode($array);
