80354b3ef5a8d18a7c9bec88bb1dfb79083a2888
DiogoFeld <43175839+DiogoFeld@users.noreply.github.com>
2018-11-18 16:31:48 -0200

    foi  o php
    AEEE
    final do java script do inbox ainda ta meio rebuscado, vou fazer um pouco dele mas não sei...
    tem umas funções "estranahs"

--- php/listar_enviados.php
+++ php/listar_enviados.php
@@ -4,22 +4,23 @@ $emails = file_get_contents("../arquivos/emails/");
 $diretorio = dir($emails);
 $array = array();
 
-//
+//seleciona cada xml
+while($file=$diretorio->read()){
+	if ($file !="." && $file!=".."{
 
-
-
-	while($file=$diretorio ->read()){
-	if ($file !="." && $file!=".."&& $_SESSION== {//remetente
-	$xml_string = file_get_contents($emails . $file);
+	$xml_string = file_get_contents($emails . $file);//carrega xml
 	$xml_object = simplexml_load_string($xml_string);
-	
-	$array = array(
-		'id'        => (string)$xml_object->id,
-  		'conteudo'  => (string)$xml_object->conteudo,
-      	'assunto'   => (string)$xml_object->assunto,
-		'remetente' => (string)$xml_object->remetente
-      	);							
+
+
+		if ((string)$xml_object->remetente == $_SESSION["email"]){//compara remetente com usuario
+			$array = array(
+			'id'        => (string)$xml_object->id,
+  			'conteudo'  => (string)$xml_object->conteudo,
+      		'assunto'   => (string)$xml_object->assunto,
+			'remetente' => (string)$xml_object->remetente//add no aray
+      		);							
 		}
+	}
 }
 	$diretorio->close();
 
